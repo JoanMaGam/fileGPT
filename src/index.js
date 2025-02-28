3ba2746944cfdae8de0895b883e805f4d2eed1ac
Joan Marti <joanmartigamez@gmail.com [core]>
2025-02-28 02:14:40 +0100

    Pequeño ajuste

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get("/", addTimeStamp, (req, res) => {
+app.get("/", (req, res) => {
     res.send("<h1>FileGPT API</h1>");
 });
 
@@ -19,5 +19,5 @@ app.use('/api/v1/users', addTimeStamp, v1UsersRouter);
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`✅ Servidor corriendo en el puerto: ${PORT}`);
+    console.log(`✅ Servidor escuchando en el puerto: ${PORT}`);
 });
