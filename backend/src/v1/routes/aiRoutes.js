4965a0ef38dec0008fc562e6cb2cba6f44369dae
Joan Marti <joanmartigamez@gmail.com [core]>
2025-03-12 18:11:54 +0100

    Modificadas rutas aiRoutes.js

--- backend/src/v1/routes/aiRoutes.js
+++ backend/src/v1/routes/aiRoutes.js
@@ -1,12 +1,12 @@
 // src/v1/routes/aiRoutes.js
 const router = require('express').Router();
-const { processFile, askAI, upload } = require('../../controllers/aiController');
+const { processFile, ask, upload } = require('../../controllers/aiController');
 const { checkToken } = require('../../helpers/middlewares');
 
 // Ruta para cargar un archivo
 router.post('/upload', checkToken, upload.single('file'), processFile);
 
 // Ruta para hacer preguntas a la IA
-router.post('/ask', checkToken, askAI);
+router.post('/ask', checkToken, ask);
 
 module.exports = router;
